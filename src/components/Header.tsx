ae6938859cb215e847dac5b20e482b7de01b0158
Mi6aRdnv <95938371+Mi6aRdnv@users.noreply.github.com>
2026-08-12 20:27:22 +0200

    feat: add theme toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 export default function Header() {
+	const [theme, setTheme] = useState("night");
+	function toggleTheme() {
+		setTheme(theme === "night" ? "day" : "night");
+		document.documentElement.setAttribute("data-theme", theme);
+	}
 	return (
 		<header className="header">
 			<h1 className="title">TODO</h1>
-			<button className="theme-toggle">
-				<img src="src\assets\images\icon-moon.svg" alt="Toggle theme" />
+			<button className="theme-toggle" onClick={toggleTheme}>
+				<img
+					src={
+						theme === "night"
+							? "src\\assets\\images\\icon-sun.svg"
+							: "src\\assets\\images\\icon-moon.svg"
+					}
+					className={`theme-icon ${theme === "night" ? "night" : "day"}`}
+					alt="Toggle theme"
+				/>
 			</button>
 		</header>
 	);
